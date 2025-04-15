906235e24e608b0de544ba2d2390dc13f0857f94
Diwash Pandey <155344419+diwashpandey@users.noreply.github.com>
2025-04-15 10:58:42 +0545

    Implemented Services and Context to the program.cs

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -1,7 +1,21 @@
+using TodoApi.Contexts;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.Configure<TodoDatabaseSettings>(builder.Configuration.GetSection("TodoDatabaseSettings"));
+
+// Add services to the container.
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+// Adding Contexts to the builder
+builder.Services.AddSingleton<TodoDBContext>();
+
+// Adding Services to the builder
+builder.Services.AddScoped<TodoService>();
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
