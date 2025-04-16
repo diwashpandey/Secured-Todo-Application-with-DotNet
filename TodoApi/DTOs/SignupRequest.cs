a4e1e5a66fe9560e34c487bc6a94f4d1ccb3aa72
Diwash Pandey <155344419+diwashpandey@users.noreply.github.com>
2025-04-16 16:50:54 +0545

    Update User model and DTOs for signup and login
    Extended User model and adjusted DTOs to support signup and login workflows.

---
+++ TodoApi/DTOs/SignupRequest.cs
@@ -0,0 +1,7 @@
+namespace TodoApi.DTOs;
+public class SignupRequest
+{
+    public string Username {get; set;} = string.Empty;
+    public string RawPassword {get; set;} = string.Empty;
+    public string Email {get; set;} = string.Empty;
+}
---
+++ TodoApi/DTOs/SignupResponse.cs
@@ -0,0 +1,7 @@
+namespace TodoApi.DTOs;
+
+public class SignupResponse
+{
+    public bool SuccessStatus {get; set;}
+    public string? MessageToClient {get; set;}
+}
