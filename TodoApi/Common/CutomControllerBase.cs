1eb7cc8fd0903c1c80f6a590b98522fee5b3c696
Diwash Pandey <155344419+diwashpandey@users.noreply.github.com>
2025-04-18 13:32:56 +0545

    Added reusable GenerateResponse
    Added GenerateResponse<TData> method in CustomControllerBase to standardize API response structure across controllers. This reduces code repetition and ensures consistent response formatting.

--- TodoApi/Common/CutomControllerBase.cs
+++ TodoApi/Common/CutomControllerBase.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using TodoApi.DTOs.ApiResponse;
 
 namespace TodoApi.Common;
 
@@ -6,5 +7,14 @@ public class CustomControllerBase : ControllerBase
 {
     protected string? GetUserId() => User.FindFirst("UserId")?.Value;
 
-
+    protected ApiResponse<TData> GenerateResponse<TData>(bool successStatus, string? messageFromServer, TData data)
+    {
+    /// Generates a standardized API response structure.
+        return new ApiResponse<TData>()
+        {
+            SuccessStatus = successStatus,
+            MessageFromServer = messageFromServer,
+            Data = data
+        };
+    }
 }
