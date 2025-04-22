fbe0b7d15fb24c446e366d28350a203010fb1c45
Diwash Pandey <155344419+diwashpandey@users.noreply.github.com>
2025-04-22 16:35:31 +0545

    Minor Fix
    Unnecessary Properties have been removed form the SignupRequest DTO class.

--- TodoApi/DTOs/UserDTOs/SignupRequest.cs
+++ TodoApi/DTOs/UserDTOs/SignupRequest.cs
@@ -4,6 +4,4 @@ public class SignupRequest
     public string Username {get; set;} = string.Empty;
     public string RawPassword {get; set;} = string.Empty;
     public string Email {get; set;} = string.Empty;
-    public string RefreshToken {get; set;} = string.Empty;
-    public DateTime RefreshTokenExpiryDate {get; set;}
 }
